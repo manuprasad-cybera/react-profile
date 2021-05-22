cea527fe4ffe191206a82ff7dc8c4dea665e17bb
manuprasad <manu.whattodo@gmail.com>
2021-05-22 21:35:10 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDkLAx3CLMi8FHZCaussVFLtL-5ychyz3U",
+    authDomain: "manuprasad-profile.firebaseapp.com",
+    projectId: "manuprasad-profile",
+    storageBucket: "manuprasad-profile.appspot.com",
+    messagingSenderId: "572746594412",
+    appId: "1:572746594412:web:fdc179b6dc2ff51ba78049",
+    measurementId: "G-64T4FRR46B"
+  };
